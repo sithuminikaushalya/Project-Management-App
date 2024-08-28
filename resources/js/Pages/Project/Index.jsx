519ffde8ba33686118b43022b4af070573819fe6
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2024-08-28 19:53:38 +0530

    create table to store projects on the page

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,4 +1,7 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constant.jsx";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 
@@ -28,15 +31,43 @@ export default function Index({ auth, projects }) {
                     <th className="px-3 py-3 text-right">Actions</th>
                   </tr>
                 </thead>
+                <thead className="text-gray-700 uppercase border-b-2 border-gray-500 text-xc bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput
+                        className="w-full"
+                        placeholder="Project Name"
+                        onBlur={e => searchFieldChanged('name', e.target.value)}
+                        onKeyPress={e => onKeyPress('name', e)}
+                      />
+                    </th>
+                    <th className="px-3 py-3">
+                      <SelectInput className="w-full" />
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                  </tr>
+                </thead>
                 <tbody>
                   {projects.data.map(project => (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 " key={project.id} >
                       <td className="px-3 py-2">{project.id}</td>
                       <td className="px-3 py-2">
                         <img src={project.image_path} style={{ width: 60 }} />
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
+                      <td className="px-3 py-2">
+                        <span className={
+                          "px-2 py-1 rounded text-white " +
+                          PROJECT_STATUS_CLASS_MAP[project.status]
+                        }>
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
                       <td className="px-3 py-2">{project.created_at}</td>
                       <td className="px-3 py-2">{project.due_date}</td>
                       <td className="px-3 py-2">{project.createdBy.name}</td>
